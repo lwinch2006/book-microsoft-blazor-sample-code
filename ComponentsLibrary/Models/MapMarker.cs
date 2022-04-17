9a7e61236affb7a844781f90fd9eff939c6ed810
D.K <6912915+lwinch2006@users.noreply.github.com>
2022-04-17 18:34:57 +0200

    Added map to pizza restaurant

---
+++ ComponentsLibrary/Models/MapMarker.cs
@@ -0,0 +1,9 @@
+namespace ComponentsLibrary.Models;
+
+public class MapMarker
+{
+    public string? Description { get; set; }
+    public double X { get; set; }
+    public double Y { get; set; }
+    public bool ShowPopup { get; set; }
+}
